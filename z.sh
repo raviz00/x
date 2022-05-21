c69e5de0ab53d23603c9cdb13d2563bbb372ef1d
raviz00 <88627125+raviz00@users.noreply.github.com>
2022-05-22 03:22:21 +0700

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #love u fika
 
-sudo apt install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+apt-get update -y
+apt install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
