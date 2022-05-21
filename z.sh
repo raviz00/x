50f559e2cf793f3138d58328d421b3d18ec067e8
raviz00 <88627125+raviz00@users.noreply.github.com>
2022-05-21 23:33:59 +0700

    Create z.sh

---
+++ z.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#love u fika
+
+sudo apt install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
